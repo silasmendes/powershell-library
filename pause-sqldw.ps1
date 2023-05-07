a6e2ab274582ab431cc2fd4b8487d5eba9b5b7f0
Silas Mendes <silasmendes@gmail.com>
2023-05-07 14:23:19 +0100

    Pause SQL DW
    We can use this script to pause SQL DW (formerly), and it works very well as a POSH script in Azure Automation to automate pause/resume. However, this doesn't work for Synapse Dedicated SQL pool because the PowerShell command is different.

---
+++ pause-sqldw.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module Az.Accounts
+Import-Module Az.Sql
+Import-Module Az.Synapse
+	
+try
+{
+	"Logging in to Azure..."
+	Connect-AzAccount -Identity
+}
+catch {
+	Write-Error -Message $_.Exception
+	throw $_.Exception
+}
+	
+$SubscriptionId = "12345678-abcd-12ab-cd34-567897123abc"
+	
+Set-AzContext $SubscriptionId
+	
+Suspend-AzSqlDatabase –ResourceGroupName "rgsqldw" –ServerName "srvsqldwstandalone" –DatabaseName "dbsqldw"
